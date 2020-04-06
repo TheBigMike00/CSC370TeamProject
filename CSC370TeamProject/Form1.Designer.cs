fa38cae50f8af1170283b57f6bae539a74ec235a
Michael Brocker <49590891+TheBigMike00@users.noreply.github.com>
2020-04-06 17:47:15 -0500

    Add project files.

---
+++ CSC370TeamProject/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace CSC370TeamProject
+{
+    partial class StockProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stock1TB = new System.Windows.Forms.TextBox();
+            this.stocksLabel = new System.Windows.Forms.Label();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.totalvalueLabel = new System.Windows.Forms.Label();
+            this.quantity1TB = new System.Windows.Forms.TextBox();
+            this.stock2TB = new System.Windows.Forms.TextBox();
+            this.quantity2TB = new System.Windows.Forms.TextBox();
+            this.stock3TB = new System.Windows.Forms.TextBox();
+            this.quantity3TB = new System.Windows.Forms.TextBox();
+            this.stock4TB = new System.Windows.Forms.TextBox();
+            this.quantity4TB = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.totalvalue1Label = new System.Windows.Forms.Label();
+            this.totalvalue2Label = new System.Windows.Forms.Label();
+            this.totalvalue3Label = new System.Windows.Forms.Label();
+            this.totalvalue4Label = new System.Windows.Forms.Label();
+            this.profiletotalLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // stock1TB
+            // 
+            this.stock1TB.Location = new System.Drawing.Point(147, 97);
+            this.stock1TB.Name = "stock1TB";
+            this.stock1TB.Size = new System.Drawing.Size(100, 26);
+            this.stock1TB.TabIndex = 0;
+            // 
+            // stocksLabel
+            // 
+            this.stocksLabel.AutoSize = true;
+            this.stocksLabel.Location = new System.Drawing.Point(164, 52);
+            this.stocksLabel.Name = "stocksLabel";
+            this.stocksLabel.Size = new System.Drawing.Size(62, 20);
+            this.stocksLabel.TabIndex = 1;
+            this.stocksLabel.Text = "STOCK";
+            this.stocksLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Location = new System.Drawing.Point(353, 52);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(89, 20);
+            this.quantityLabel.TabIndex = 2;
+            this.quantityLabel.Text = "QUANTITY";
+            this.quantityLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalvalueLabel
+            // 
+            this.totalvalueLabel.AutoSize = true;
+            this.totalvalueLabel.Location = new System.Drawing.Point(553, 52);
+            this.totalvalueLabel.Name = "totalvalueLabel";
+            this.totalvalueLabel.Size = new System.Drawing.Size(117, 20);
+            this.totalvalueLabel.TabIndex = 3;
+            this.totalvalueLabel.Text = "TOTAL VALUE";
+            // 
+            // quantity1TB
+            // 
+            this.quantity1TB.Location = new System.Drawing.Point(342, 97);
+            this.quantity1TB.Name = "quantity1TB";
+            this.quantity1TB.Size = new System.Drawing.Size(100, 26);
+            this.quantity1TB.TabIndex = 4;
+            // 
+            // stock2TB
+            // 
+            this.stock2TB.Location = new System.Drawing.Point(147, 144);
+            this.stock2TB.Name = "stock2TB";
+            this.stock2TB.Size = new System.Drawing.Size(100, 26);
+            this.stock2TB.TabIndex = 6;
+            // 
+            // quantity2TB
+            // 
+            this.quantity2TB.Location = new System.Drawing.Point(342, 144);
+            this.quantity2TB.Name = "quantity2TB";
+            this.quantity2TB.Size = new System.Drawing.Size(100, 26);
+            this.quantity2TB.TabIndex = 7;
+            // 
+            // stock3TB
+            // 
+            this.stock3TB.Location = new System.Drawing.Point(147, 196);
+            this.stock3TB.Name = "stock3TB";
+            this.stock3TB.Size = new System.Drawing.Size(100, 26);
+            this.stock3TB.TabIndex = 9;
+            // 
+            // quantity3TB
+            // 
+            this.quantity3TB.Location = new System.Drawing.Point(342, 196);
+            this.quantity3TB.Name = "quantity3TB";
+            this.quantity3TB.Size = new System.Drawing.Size(100, 26);
+            this.quantity3TB.TabIndex = 10;
+            // 
+            // stock4TB
+            // 
+            this.stock4TB.Location = new System.Drawing.Point(147, 246);
+            this.stock4TB.Name = "stock4TB";
+            this.stock4TB.Size = new System.Drawing.Size(100, 26);
+            this.stock4TB.TabIndex = 12;
+            // 
+            // quantity4TB
+            // 
+            this.quantity4TB.Location = new System.Drawing.Point(342, 245);
+            this.quantity4TB.Name = "quantity4TB";
+            this.quantity4TB.Size = new System.Drawing.Size(100, 26);
+            this.quantity4TB.TabIndex = 13;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(342, 307);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(108, 34);
+            this.enterButton.TabIndex = 15;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // totalvalue1Label
+            // 
+            this.totalvalue1Label.AutoSize = true;
+            this.totalvalue1Label.Location = new System.Drawing.Point(563, 97);
+            this.totalvalue1Label.Name = "totalvalue1Label";
+            this.totalvalue1Label.Size = new System.Drawing.Size(49, 20);
+            this.totalvalue1Label.TabIndex = 16;
+            this.totalvalue1Label.Text = "$0.00";
+            // 
+            // totalvalue2Label
+            // 
+            this.totalvalue2Label.AutoSize = true;
+            this.totalvalue2Label.Location = new System.Drawing.Point(563, 150);
+            this.totalvalue2Label.Name = "totalvalue2Label";
+            this.totalvalue2Label.Size = new System.Drawing.Size(49, 20);
+            this.totalvalue2Label.TabIndex = 17;
+            this.totalvalue2Label.Text = "$0.00";
+            // 
+            // totalvalue3Label
+            // 
+            this.totalvalue3Label.AutoSize = true;
+            this.totalvalue3Label.Location = new System.Drawing.Point(563, 199);
+            this.totalvalue3Label.Name = "totalvalue3Label";
+            this.totalvalue3Label.Size = new System.Drawing.Size(49, 20);
+            this.totalvalue3Label.TabIndex = 18;
+            this.totalvalue3Label.Text = "$0.00";
+            // 
+            // totalvalue4Label
+            // 
+            this.totalvalue4Label.AutoSize = true;
+            this.totalvalue4Label.Location = new System.Drawing.Point(563, 248);
+            this.totalvalue4Label.Name = "totalvalue4Label";
+            this.totalvalue4Label.Size = new System.Drawing.Size(49, 20);
+            this.totalvalue4Label.TabIndex = 19;
+            this.totalvalue4Label.Text = "$0.00";
+            // 
+            // profiletotalLabel
+            // 
+            this.profiletotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.profiletotalLabel.Location = new System.Drawing.Point(228, 367);
+            this.profiletotalLabel.Name = "profiletotalLabel";
+            this.profiletotalLabel.Size = new System.Drawing.Size(350, 23);
+            this.profiletotalLabel.TabIndex = 20;
+            this.profiletotalLabel.Text = "Profile Total: $0.00";
+            this.profiletotalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // StockProfile
+            // 
+            this.AcceptButton = this.enterButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.profiletotalLabel);
+            this.Controls.Add(this.totalvalue4Label);
+            this.Controls.Add(this.totalvalue3Label);
+            this.Controls.Add(this.totalvalue2Label);
+            this.Controls.Add(this.totalvalue1Label);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.quantity4TB);
+            this.Controls.Add(this.stock4TB);
+            this.Controls.Add(this.quantity3TB);
+            this.Controls.Add(this.stock3TB);
+            this.Controls.Add(this.quantity2TB);
+            this.Controls.Add(this.stock2TB);
+            this.Controls.Add(this.quantity1TB);
+            this.Controls.Add(this.totalvalueLabel);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.stocksLabel);
+            this.Controls.Add(this.stock1TB);
+            this.Name = "StockProfile";
+            this.Text = "StockProfile";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox stock1TB;
+        private System.Windows.Forms.Label stocksLabel;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.Label totalvalueLabel;
+        private System.Windows.Forms.TextBox quantity1TB;
+        private System.Windows.Forms.TextBox stock2TB;
+        private System.Windows.Forms.TextBox quantity2TB;
+        private System.Windows.Forms.TextBox stock3TB;
+        private System.Windows.Forms.TextBox quantity3TB;
+        private System.Windows.Forms.TextBox stock4TB;
+        private System.Windows.Forms.TextBox quantity4TB;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label totalvalue1Label;
+        private System.Windows.Forms.Label totalvalue2Label;
+        private System.Windows.Forms.Label totalvalue3Label;
+        private System.Windows.Forms.Label totalvalue4Label;
+        private System.Windows.Forms.Label profiletotalLabel;
+    }
+}
+
---
+++ CSC370TeamProject/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AlphaVantage.Net.Stocks;
+using AlphaVantage.Net.Stocks.TimeSeries;
+
+namespace CSC370TeamProject
+{
+    public partial class StockProfile : Form
+    {
+
+        /*Test for api working
+         * Derek R. Michael B. Justin J. 
+         * used Alpha Vantage Api
+         * Code from https://github.com/LutsenkoKirill/AlphaVantage.Net
+        */
+
+        string[] stocks = new string[4];
+        int[] quantities = new int[4];
+        double[] currPrice = new double[4]; 
+
+        public StockProfile()
+        {
+            InitializeComponent();
+
+        }
+
+        public async Task AlphaVantageStocksDemo()
+        {
+            string apiKey = "S5TJJRN8PSP31YVU"; // enter your API key here
+
+            var client = new AlphaVantageStocksClient(apiKey);
+            for (int i = 0; i < 4; i++)
+            {
+                // retrieve daily time series for stocks of Apple Inc.:
+                if(stocks[i] == null)
+                {
+                    continue;
+                }
+                StockTimeSeries timeSeries = await client.RequestDailyTimeSeriesAsync(stocks[i].ToString(),
+                    TimeSeriesSize.Compact, adjusted: false);
+
+                foreach (var dataPoint in timeSeries.DataPoints)
+                {
+                    //Console.WriteLine($"{dataPoint.Time}: {dataPoint.ClosingPrice}");
+                    currPrice[i] = (double)(dataPoint.ClosingPrice);
+                    Console.WriteLine(currPrice[i]);
+                    //Break used to only acess one data entry or else gets past 100 days
+                    break;
+                }
+            }
+        }
+
+        public void defineLabels()
+        {
+            if (stock1TB.Text != null)
+            {
+                totalvalue1Label.Text = "$" + (quantities[0] * currPrice[0]);
+            }
+            else { currPrice[0] = 0; }
+            if (stock2TB.Text != null)
+            {
+                totalvalue2Label.Text = "$" + (quantities[1] * currPrice[1]);
+            }
+            else { currPrice[1] = 0; }
+            if (stock3TB.Text != null)
+            {
+                totalvalue3Label.Text = "$" + (quantities[2] * currPrice[2]);
+            }
+            else { currPrice[2] = 0; }
+            if (stock4TB.Text != null)
+            {
+                totalvalue4Label.Text = "$" + (quantities[3] * currPrice[3]);
+            }
+            else { currPrice[3] = 0; }
+            profiletotalLabel.Text = "Profile Total: $" + getProfileTotal();
+        }
+
+        public void fillArrayLists()
+        {
+            try
+            {
+                if (stock1TB.Text != null && quantity1TB.Text != null)
+                {
+                    stocks[0] = (stock1TB.Text);
+                    quantities[0] = Convert.ToInt32(quantity1TB.Text);
+                }
+                if (stock2TB.Text != null && quantity2TB.Text != null)
+                {
+                    stocks[1] = (stock2TB.Text);
+                    quantities[1] = Convert.ToInt32(quantity2TB.Text);
+                }
+                if (stock3TB.Text != null && quantity3TB.Text != null)
+                {
+                    stocks[2] = (stock3TB.Text);
+                    quantities[2] = Convert.ToInt32(quantity3TB.Text);
+                }
+                if (stock4TB.Text != null && quantity4TB.Text != null)
+                {
+                    stocks[3] = (stock4TB.Text);
+                    quantities[3] = Convert.ToInt32(quantity4TB.Text);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Please double check inputs. Ensure stock" +
+                    " symbols are typed correctly and entered stocks have a quantitiy.", "Error On Input");
+            }
+        }
+
+        public double getProfileTotal()
+        {
+            double valToReturn = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                valToReturn += (quantities[i] * currPrice[i]);
+            }
+            return valToReturn;
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            fillArrayLists();
+            _ = AlphaVantageStocksDemo();
+            defineLabels();
+        }
+    }
+}
